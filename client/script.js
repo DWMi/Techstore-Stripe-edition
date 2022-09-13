62fab680677f4ed3f8964e4b50fc0dd9cef34976
Julius Djudjaku <juliusdjudjaku@gmail.com>
2022-09-13 13:19:55 +0200

    In progress

--- client/script.js
+++ client/script.js
@@ -1,27 +1,44 @@
 var itemsData;
+let prices;
 var shoppingCart = [];
 var isItemsViewVisible = false;
 
 /* Fetch data from the json file into a javascript object */
-fetch("./assets/data.json")
-.then(function(response) {
-    return response.json();
-})
-.then(function(data) {
-    itemsData = data;
-    createUIFromLoadedItemsData();
-});
+fetch("http://localhost:3000/products")
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        itemsData = data;
+        console.log(itemsData)
+        createUIFromLoadedItemsData();
+    });
+
+// FETCH PRICES
+
+fetch("http://localhost:3000/prices")
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        prices = data;
+        console.log(prices.data[0].product)
+    });
 
 /* Use the data to create a list of these object on your website */
 function createUIFromLoadedItemsData() {
     if (isItemsViewVisible) { return; }
     isItemsViewVisible = true;
-    
+
     /* Create a list of the products */
     var list = document.createElement("ul");
-    for(var index = 0; index < itemsData.length; index++) {
-        list.appendChild(createListItem(itemsData[index]));
-    }
+
+  
+   
+    itemsData.data.forEach(product => {
+        list.appendChild(createListItem(product))
+    });
+
 
     /* Add the list to the DOM */
     var container = document.querySelector("#main");
@@ -32,28 +49,37 @@ function createUIFromLoadedItemsData() {
     }
 }
 
-function createListItem(itemData) {
+function createListItem(product) {
+   
+ 
     /* Title */
     var title = document.createElement("h3");
-    title.innerText = itemData.title;
-    
+    title.innerText = product.name;
+
     /* Description */
     var description = document.createElement("p");
-    description.innerText = itemData.description;
-    
+    description.innerText = product.description
+
     /* Image */
     var image = document.createElement("img");
-    image.src = "./assets/" + itemData.image;
+    image.src = product.images[0]
 
+    priceArr = prices.data
+
+/*     const productPrice = prices.data.find(price => price.product == product.id)
+ */
+    console.log(priceArr)
     /* Price */
-    var price = document.createElement("span");
-    price.innerText = "" + itemData.price + " kr";
     
+    var price = document.createElement("span");
+    price.innerText = '100lkr'
+
     /* Button */
     var button = document.createElement("button");
     button.innerHTML = '<i class="fa fa-cart-arrow-down" aria-hidden="true"></i>' + "&nbsp;&nbsp;&nbsp;" + "Lägg till i kundvagnen";
-    button.onclick = function() {
-        shoppingCart.push(itemData);
+    button.onclick = function () {
+        shoppingCart.push(product);
+        console.log(shoppingCart)
         counter = document.querySelector("#counter");
         counter.innerText = shoppingCart.length;
     };
@@ -76,16 +102,16 @@ function showShoppingCart() {
     /* Header */
     var header = document.createElement("h2");
     header.innerHTML = '<i class="fa fa-shopping-cart" aria-hidden="true"></i>' + " Kundvagn";
-    
+
     /* Shopping list */
     var list = document.createElement("ul");
-    for(var index = 0; index < shoppingCart.length; index++) {
+    for (var index = 0; index < shoppingCart.length; index++) {
         list.appendChild(createShoppingCartItem(shoppingCart[index], index));
     }
-    
+
     /* Shopping info & action */
     var info = createShoppingSummary();
-    
+
     var content = document.createElement("div");
     content.appendChild(header);
     content.appendChild(list);
@@ -107,11 +133,11 @@ function createShoppingCartItem(itemData, index) {
     /* Price */
     var price = document.createElement("span");
     price.innerText = "" + itemData.price + " kr";
-    
+
     /* Button */
     var button = document.createElement("button");
     button.innerHTML = '<i class="fa fa-trash-o" aria-hidden="true"></i>' + "&nbsp;&nbsp;&nbsp;" + "Ta bort";
-    button.onclick = function() {
+    button.onclick = function () {
         /* Remove the item from the array */
         shoppingCart.splice(index, 1);
         /* Update the counter */
@@ -134,16 +160,16 @@ function createShoppingCartItem(itemData, index) {
 function createShoppingSummary() {
     /* Total price */
     var totalPrice = 0;
-    for(var i = 0; i < shoppingCart.length; i++) {
+    for (var i = 0; i < shoppingCart.length; i++) {
         totalPrice += shoppingCart[i].price;
     }
     var priceLabel = document.createElement("h2");
     priceLabel.innerText = "Totalt pris: " + totalPrice + " kr";
-    
+
     /* Proceed button */
     var proceedButton = document.createElement("button");
     proceedButton.innerHTML = '<i class="fa fa-check" aria-hidden="true"></i>' + "&nbsp;&nbsp;&nbsp;" + "Slutför ditt köp";
-    proceedButton.onclick = function() {
+    proceedButton.onclick = function () {
         alert("Tack för din beställning, vi önskar dig en fin kväll! Ses snart igen =)");
     };
 
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 import express from "express";
+import dotenv from 'dotenv'
+dotenv.config()
 import fetch from "node-fetch";
 import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_PRIVATE_KEY);
 
 const port = 3000;
 
@@ -10,10 +14,29 @@ app.use(express.json());
 app.use("/", express.static("./client"));
 
 
+app.get('/products', async (req, res) => {
+  const products = await stripe.products.list({
+  
+  });
+
+  res.json(products)
+
+})
+
+app.get('/prices', async (req, res) => {
+  const prices = await stripe.prices.list({
+    
+  });
+
+  res.json(prices)
+
+})
+
 
 
 
 
 app.listen(port, () => {
   console.log("Server is running on port " + port);
+  console.log(process.env.STRIPE_PRIVATE_KEY);
 });
