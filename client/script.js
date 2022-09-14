3e3d118d9e0a9f0572275ccb233bea938daed614
Julius Djudjaku <juliusdjudjaku@gmail.com>
2022-09-14 11:22:35 +0200

    local-storage fix

--- client/script.js
+++ client/script.js
@@ -1,6 +1,11 @@
 var itemsData;
 let priceArr;
-var shoppingCart = [];
+let shoppingCart;
+if(localStorage.getItem('products')) {
+     shoppingCart = JSON.parse(localStorage.getItem('products'))
+}else{
+     shoppingCart = [];
+}
 var isItemsViewVisible = false;
 
 /* Fetch data from the json file into a javascript object */
@@ -26,6 +31,10 @@ function getProductPrices() {
             console.log(priceArr)
             createUIFromLoadedItemsData();
         });
+
+        let totalQty = localStorage.getItem('quantity')
+        counter = document.querySelector("#counter");
+        counter.innerText = totalQty 
 }
 
 
@@ -34,11 +43,11 @@ function getProductPrices() {
 function createUIFromLoadedItemsData() {
     if (isItemsViewVisible) { return; }
     isItemsViewVisible = true;
-    
+
     /* Create a list of the products */
     var list = document.createElement("ul");
 
-   
+
     itemsData.data.forEach(product => {
         let priceObj = priceArr.find(price => price.id == product.default_price)
         list.appendChild(createListItem(product, priceObj))
@@ -55,25 +64,23 @@ function createUIFromLoadedItemsData() {
 }
 
 function createListItem(product, priceObj) {
-    
+
     /* Title */
     var title = document.createElement("h3");
     title.innerText = product.name;
-    
+
     /* Description */
     var description = document.createElement("p");
     description.innerText = product.description;
-    
+
     /* Image */
     var image = document.createElement("img");
     image.src = product.images[0]
 
-
-
     /* Price */
 
     let priceString = priceObj.unit_amount.toString()
-    if(priceObj) {
+    if (priceObj) {
         var price = document.createElement("span");
         price.innerText = `${priceString.substring(0, priceString.length - 2)} kr`
     }
@@ -81,10 +88,33 @@ function createListItem(product, priceObj) {
     /* Button */
     var button = document.createElement("button");
     button.innerHTML = '<i class="fa fa-cart-arrow-down" aria-hidden="true"></i>' + "&nbsp;&nbsp;&nbsp;" + "Lägg till i kundvagnen";
-    button.onclick = function() {
-        shoppingCart.push(product);
-        counter = document.querySelector("#counter");
-        counter.innerText = shoppingCart.length;
+
+    button.onclick = function () {
+        localStorage.setItem('products', JSON.stringify(shoppingCart))
+        let totalQty = localStorage.getItem('quantity')
+        totalQty = parseInt(totalQty)
+        let exist = shoppingCart.find(item => item.id == product.id)
+
+        //Add to localStorage cart
+        if (exist) {
+            exist.qty += 1
+            localStorage.setItem('products', JSON.stringify(shoppingCart))
+        } else {
+            shoppingCart.push({ ...product, qty: 1, price: priceObj.unit_amount});
+            localStorage.setItem('products', JSON.stringify(shoppingCart))
+        }
+
+        //add to totalQty in cart
+        if (totalQty) {
+            counter = document.querySelector("#counter");
+            localStorage.setItem('quantity', totalQty + 1)
+            counter.innerText = totalQty + 1
+        } else {
+            localStorage.setItem('quantity', 1)
+            counter = document.querySelector("#counter");
+            counter.innerText = 1
+        }
+
     };
 
     var item = document.createElement("li");
@@ -105,16 +135,18 @@ function showShoppingCart() {
     /* Header */
     var header = document.createElement("h2");
     header.innerHTML = '<i class="fa fa-shopping-cart" aria-hidden="true"></i>' + " Kundvagn";
-    
+
     /* Shopping list */
     var list = document.createElement("ul");
-    for(var index = 0; index < shoppingCart.length; index++) {
-        list.appendChild(createShoppingCartItem(shoppingCart[index], index));
-    }
-    
+
+    shoppingCart = JSON.parse(localStorage.getItem('products'))
+    shoppingCart.forEach(product => {
+        list.appendChild(createShoppingCartItem(product));
+    });
+ 
     /* Shopping info & action */
     var info = createShoppingSummary();
-    
+
     var content = document.createElement("div");
     content.appendChild(header);
     content.appendChild(list);
@@ -124,37 +156,68 @@ function showShoppingCart() {
     container.replaceChild(content, container.firstChild);
 }
 
-function createShoppingCartItem(itemData, index) {
+function createShoppingCartItem(product) {
     /* Image */
     var image = document.createElement("img");
-    image.src = "./assets/" + itemData.image;
+    image.src = product.images[0]
 
     /* Title */
     var title = document.createElement("h3");
-    title.innerText = itemData.title;
+    title.innerText = product.name
 
     /* Price */
     var price = document.createElement("span");
-    price.innerText = "" + itemData.price + " kr";
-    
+    price.innerText = product.price/100 * product.qty + " " + "kr"
+
+    /* Qty */
+    let qty = document.createElement('span')
+    qty.innerText = `Antal: ${product.qty}`
+
     /* Button */
     var button = document.createElement("button");
     button.innerHTML = '<i class="fa fa-trash-o" aria-hidden="true"></i>' + "&nbsp;&nbsp;&nbsp;" + "Ta bort";
-    button.onclick = function() {
+    button.onclick = function () {
         /* Remove the item from the array */
-        shoppingCart.splice(index, 1);
-        /* Update the counter */
-        counter = document.querySelector("#counter");
-        counter.innerText = shoppingCart.length;
-        /* Update the UI list */
+        shoppingCart = JSON.parse(localStorage.getItem('products'))
+        let totalQty = localStorage.getItem('quantity')
+        totalQty = parseInt(totalQty)
+
+        let exist = shoppingCart.find(item => item.id == product.id)
+        if (exist.qty > 1) {
+            exist.qty -= 1
+            localStorage.setItem('products', JSON.stringify(shoppingCart))
+            
+        } else {
+            let indexToRemove = shoppingCart.findIndex(item => item.id == product.id)
+            shoppingCart.splice(indexToRemove, 1)
+            localStorage.setItem('products', JSON.stringify(shoppingCart))
+        }
+
+         if (totalQty) {
+            counter = document.querySelector("#counter");
+            localStorage.setItem('quantity', totalQty - 1)
+            counter.innerText = totalQty - 1
+        } else {
+            localStorage.setItem('quantity', )
+            counter = document.querySelector("#counter");
+            counter.innerText = totalQty
+        }
+        
+        
         isItemsViewVisible = true;
         showShoppingCart();
+
+        /* Update the counter */
+        /* counter = document.querySelector("#counter");
+        counter.innerText = totalQty */
+        /* Update the UI list */
     };
 
     var item = document.createElement("li");
     item.appendChild(image);
     item.appendChild(title);
     item.appendChild(price);
+    item.appendChild(qty)
     item.appendChild(button);
 
     return item;
@@ -162,18 +225,22 @@ function createShoppingCartItem(itemData, index) {
 
 function createShoppingSummary() {
     /* Total price */
+    shoppingCart = JSON.parse(localStorage.getItem('products'))
+
     var totalPrice = 0;
-    for(var i = 0; i < shoppingCart.length; i++) {
-        totalPrice += shoppingCart[i].price;
+    for (var i = 0; i < shoppingCart.length; i++) {
+        totalPrice += shoppingCart[i].price * shoppingCart[i].qty /100;
     }
     var priceLabel = document.createElement("h2");
     priceLabel.innerText = "Totalt pris: " + totalPrice + " kr";
-    
+
     /* Proceed button */
     var proceedButton = document.createElement("button");
     proceedButton.innerHTML = '<i class="fa fa-check" aria-hidden="true"></i>' + "&nbsp;&nbsp;&nbsp;" + "Slutför ditt köp";
-    proceedButton.onclick = function() {
-        alert("Tack för din beställning, vi önskar dig en fin kväll! Ses snart igen =)");
+    proceedButton.onclick = function () {
+        /* alert("Tack för din beställning, vi önskar dig en fin kväll! Ses snart igen =)"); */
+
+        console.log(shoppingCart)
     };
 
     var info = document.createElement("div");
