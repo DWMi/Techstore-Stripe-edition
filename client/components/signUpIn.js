e89bea5c1d735de964ea7737a3c12692a55be063
Julius Djudjaku <juliusdjudjaku@gmail.com>
2022-09-21 10:10:09 +0200

    fetch orders to myPages and error handling when logIn

--- client/components/signUpIn.js
+++ client/components/signUpIn.js
@@ -1,23 +1,15 @@
 let testRes = await checkLogIn()
 
-
 async function checkLogIn() {
     let url = "http://localhost:3000/checkLogin"
     let method = "GET"
     let result = await makeRequest(url, method, undefined)
-    /* fetch("http://localhost:3000/checkLogin")
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-    test = data
-    }); */
     return result;
 
 }
 
 
-console.log(testRes.name)
+console.log(testRes)
 
 const rightItems = document.querySelector('.rightItems'),
     accountStuff = document.createElement('div'),
@@ -50,7 +42,7 @@ signInBtn.classList.add('signInBtn')
 signUpBtn.classList.add('signUpBtn')
 signOutBtn.classList.add('signOutBtn')
 
-userBtn.innerHTML = '<i class="fa fa-user-circle" aria-hidden="true"></i>'  
+userBtn.innerHTML = '<i class="fa fa-user-circle" aria-hidden="true"></i>'
 signInBtn.innerText = 'Sign In'
 signUpBtn.innerText = 'Sign Up'
 signOutBtn.innerText = 'Logout'
@@ -72,7 +64,7 @@ loginStuff.append(signUpBtn)
 loginStuff.append(signInBtn)
 loginStuff.append(signOutBtn)
 
-if(testRes.loggedIn) {
+if (testRes.loggedIn) {
     loginStuff.removeChild(signInBtn)
     loginStuff.removeChild(signUpBtn)
 } else {
@@ -81,7 +73,7 @@ if(testRes.loggedIn) {
     loginStuff.removeChild(signOutBtn)
 }
 
-const signUpForm =()=>{
+const signUpForm = () => {
 
     const signUpFormCon = document.createElement('div'),
         signUpCover = document.createElement('div'),
@@ -94,99 +86,104 @@ const signUpForm =()=>{
         exitSignUpForm = document.createElement('div'),
         signUpAddress = document.createElement('input'),
         signUpZip = document.createElement('input'),
-        signUpCity = document.createElement('input')
-        
-        
-     
-        signUpEmail.classList.add('signUpEmail')
-        signUpCover.classList.add('signUpCover')
-        signUpFormCon.classList.add('signUpFormCon')
-        signUpForm.classList.add('signUpForm')
-        signUpUsername.classList.add('signUpUsername')
-        signUpPassword.classList.add('signUpPassword')
-        signUpSubmitBtn.classList.add('signUpSubmitBtn')
-        exitSignUpFormCon.classList.add('exitSignUpFormCon')
-        exitSignUpForm.classList.add('exitSignUpForm')
-        signUpAddress.classList.add('signUpAddress')
-        signUpZip.classList.add('signUpZip')
-        signUpCity.classList.add('signUpCity')
-        
-        
-        body.append(signUpCover)
-        signUpCover.append(signUpFormCon)
-        signUpFormCon.append(signUpForm)
-        signUpForm.append(exitSignUpFormCon)
-        exitSignUpFormCon.append(exitSignUpForm)
-        signUpForm.append(signUpUsername)
-        signUpForm.append(signUpPassword)
-        signUpForm.append(signUpEmail)
-        signUpForm.append(signUpAddress)
-        signUpForm.append(signUpZip)
-        signUpForm.append(signUpCity)
-        signUpForm.append(signUpSubmitBtn)  
+        signUpCity = document.createElement('input'),
+        errorMsg = document.createElement('p')
 
-        exitSignUpForm.innerText ='❌'
-        signUpUsername.placeholder = 'Full name'
-        signUpPassword.placeholder = 'Password'
-        signUpEmail.placeholder = 'Email'
-        signUpAddress.placeholder = 'Address'
-        signUpZip.placeholder ='Zipcode'
-        signUpEmail.type ='email'
-        signUpPassword.type = 'Password'
-        signUpAddress.type = 'Address'
-        signUpZip.type ='text'
-        signUpCity.placeholder = 'City'
-        signUpEmail.setAttribute('required','')
-        signUpUsername.setAttribute('required','')
-        signUpPassword.setAttribute('required','')
-        signUpAddress.setAttribute('required','')
-        signUpCity.setAttribute('required','')
-        signUpZip.setAttribute('required','')
-        signUpZip.setAttribute('maxLength','5')
-        signUpZip.setAttribute('minLength','5')
-        signUpSubmitBtn.innerHTML = 'Sign Up'
-        signUpSubmitBtn.type = 'submit'
-        main.style.filter='blur(8px)'
-        body.style.overflowY='hidden'
-        navbar.style.filter='blur(8px)'
+    errorMsg.classList.add('errorMsg')
+    signUpEmail.classList.add('signUpEmail')
+    signUpCover.classList.add('signUpCover')
+    signUpFormCon.classList.add('signUpFormCon')
+    signUpForm.classList.add('signUpForm')
+    signUpUsername.classList.add('signUpUsername')
+    signUpPassword.classList.add('signUpPassword')
+    signUpSubmitBtn.classList.add('signUpSubmitBtn')
+    exitSignUpFormCon.classList.add('exitSignUpFormCon')
+    exitSignUpForm.classList.add('exitSignUpForm')
+    signUpAddress.classList.add('signUpAddress')
+    signUpZip.classList.add('signUpZip')
+    signUpCity.classList.add('signUpCity')
 
-        exitSignUpForm.addEventListener('click',()=>{
+
+    body.append(signUpCover)
+    signUpCover.append(signUpFormCon)
+    signUpFormCon.append(signUpForm)
+    signUpForm.append(exitSignUpFormCon)
+    exitSignUpFormCon.append(exitSignUpForm)
+    signUpForm.append(signUpUsername, signUpPassword, signUpEmail, signUpAddress, signUpZip, signUpCity, errorMsg, signUpSubmitBtn)
+ 
+
+    exitSignUpForm.innerText = '❌'
+    signUpUsername.placeholder = 'Full name'
+    signUpPassword.placeholder = 'Password'
+    signUpEmail.placeholder = 'Email'
+    signUpAddress.placeholder = 'Address'
+    signUpZip.placeholder = 'Zipcode'
+    signUpEmail.type = 'email'
+    signUpPassword.type = 'Password'
+    signUpAddress.type = 'Address'
+    signUpZip.type = 'text'
+    signUpCity.placeholder = 'City'
+    signUpEmail.setAttribute('required', '')
+    signUpUsername.setAttribute('required', '')
+    signUpPassword.setAttribute('required', '')
+    signUpAddress.setAttribute('required', '')
+    signUpCity.setAttribute('required', '')
+    signUpZip.setAttribute('required', '')
+    signUpZip.setAttribute('maxLength', '5')
+    signUpZip.setAttribute('minLength', '5')
+    signUpSubmitBtn.innerHTML = 'Sign Up'
+    signUpSubmitBtn.type = 'submit'
+    main.style.filter = 'blur(8px)'
+    body.style.overflowY = 'hidden'
+    navbar.style.filter = 'blur(8px)'
+
+    exitSignUpForm.addEventListener('click', () => {
         body.removeChild(signUpCover)
-        main.style.filter=''
-        body.style.overflowY=''
-        navbar.style.filter=''
-        })
+        main.style.filter = ''
+        body.style.overflowY = ''
+        navbar.style.filter = ''
+    })
 
-       signUpForm.addEventListener('submit', (event) => {
-            event.preventDefault()
-            let userObj = {
-                username: signUpUsername.value,
-                password: signUpPassword.value,
-                email: signUpEmail.value,
-                address: signUpAddress.value,
-                city: signUpCity.value,
-                zip: signUpZip.value
-
-            }
-            fetch("http://localhost:3000/register", {
-                method: "POST",
+    signUpForm.addEventListener('submit', async (e) => {
+        e.preventDefault()
+        let userObj = {
+            username: signUpUsername.value,
+            password: signUpPassword.value,
+            email: signUpEmail.value,
+            address: signUpAddress.value,
+            city: signUpCity.value,
+            zip: signUpZip.value
+        }
+        try {
+            let response = await fetch("http://localhost:3000/register", {
+                method: 'POST',
                 headers: {
-                  "Content-Type": "application/json",
-                }, 
+                    "Content-Type": "application/json"
+                },
                 body: JSON.stringify(userObj)
-                
-              })
-                .then(res => res.json())
-                .then((data) => console.log(data))
-                .then(setTimeout(() => {
+            })
+            let result = await response.json()
+            console.log(result)
+            if (response.status == 401) {
+                errorMsg.innerText = result;
+                return
+            } else {
+                setTimeout(() => {
                     location.reload()
-                }, 1000))
+                }, 1000)
+            }
+        } catch (err) {
+            console.error(err);
+        }
+
         
-        })
+      
+
+    })
 
 }
 
-const signInForm = ()=>{
+const signInForm = () => {
 
     const signInFormCon = document.createElement('div'),
         signInCover = document.createElement('div'),
@@ -195,99 +192,100 @@ const signInForm = ()=>{
         signInPassword = document.createElement('input'),
         signInSubmitBtn = document.createElement('button'),
         exitSignInFormCon = document.createElement('div'),
-        exitSignInForm = document.createElement('div')
+        exitSignInForm = document.createElement('div'),
+        errorMsg = document.createElement('p')
+
+    errorMsg.classList.add('errorMsg')
+    signInCover.classList.add('signInCover')
+    signInFormCon.classList.add('signInFormCon')
+    signInForm.classList.add('signInForm')
+    signInUsername.classList.add('signInUsername')
+    signInPassword.classList.add('signInPassword')
+    signInSubmitBtn.classList.add('signInSubmitBtn')
+    exitSignInFormCon.classList.add('exitSignInFormCon')
+    exitSignInForm.classList.add('exitSignInForm')
 
 
-        signInCover.classList.add('signInCover')
-        signInFormCon.classList.add('signInFormCon')
-        signInForm.classList.add('signInForm')
-        signInUsername.classList.add('signInUsername')
-        signInPassword.classList.add('signInPassword')
-        signInSubmitBtn.classList.add('signInSubmitBtn')
-        exitSignInFormCon.classList.add('exitSignInFormCon')
-        exitSignInForm.classList.add('exitSignInForm')
-        
-        
-        body.append(signInCover)
-        signInCover.append(signInFormCon)
-        signInFormCon.append(signInForm)
-        signInForm.append(exitSignInFormCon)
-        exitSignInFormCon.append(exitSignInForm)
-        signInForm.append(signInUsername)
-        signInForm.append(signInPassword)
-        signInForm.append(signInSubmitBtn)  
+    body.append(signInCover)
+    signInCover.append(signInFormCon)
+    signInFormCon.append(signInForm)
+    signInForm.append(exitSignInFormCon)
+    exitSignInFormCon.append(exitSignInForm)
+    signInForm.append(signInUsername, signInPassword, errorMsg, signInSubmitBtn)
 
+    exitSignInForm.innerText = '❌'
+    signInUsername.placeholder = 'Email'
+    signInPassword.placeholder = 'Password'
+    signInPassword.type = 'Password'
+    signInUsername.type = 'email'
+    signInUsername.setAttribute('required', '')
+    signInPassword.setAttribute('required', '')
+    signInSubmitBtn.innerHTML = 'Sign In'
+    main.style.filter = 'blur(8px)'
+    body.style.overflowY = 'hidden'
+    navbar.style.filter = 'blur(8px)'
 
-        exitSignInForm.innerText ='❌'
-        signInUsername.placeholder = 'Email'
-        signInPassword.placeholder = 'Password'
-        signInPassword.type = 'Password'
-        signInUsername.type = 'email'
-        signInUsername.setAttribute('required','')
-        signInPassword.setAttribute('required','')
-        signInSubmitBtn.innerHTML = 'Sign In'
-        main.style.filter='blur(8px)'
-        body.style.overflowY='hidden'
-        navbar.style.filter='blur(8px)'
-
-        exitSignInForm.addEventListener('click',()=>{
+    exitSignInForm.addEventListener('click', () => {
         body.removeChild(signInCover)
-        main.style.filter=''
-        body.style.overflowY=''
-        navbar.style.filter=''
-        })
+        main.style.filter = ''
+        body.style.overflowY = ''
+        navbar.style.filter = ''
+    })
 
-        signInForm.addEventListener('submit', (e) => {
-            e.preventDefault()
-            let userObj = {
-                email: signInUsername.value,
-                password: signInPassword.value
-            }
-            fetch("http://localhost:3000/login", {
-                method: "POST",
+    signInForm.addEventListener('submit', async (e) => {
+        e.preventDefault()
+        let userObj = {
+            email: signInUsername.value,
+            password: signInPassword.value
+        }
+
+        try {
+            let response = await fetch("http://localhost:3000/login", {
+                method: 'POST',
                 headers: {
-                  "Content-Type": "application/json",
-                }, 
+                    "Content-Type": "application/json"
+                },
                 body: JSON.stringify(userObj)
-                
-              })
-                .then(res => res.json())
-                .then((data) => console.log(data))
-                .then(setTimeout(() => {
+            })
+            let result = await response.json()
+            if (response.status == 401) {
+                errorMsg.innerText = result;
+                return
+            } else {
+                setTimeout(() => {
                     location.reload()
-                }, 1000))
-                .catch((err) => {
-                    err.status
-                    err.message
-                })
-                
+                }, 1000)
+            }
+        } catch (err) {
+            console.error(err);
+        }
 
-        })
-}   
+    })
+}
 
 
-signInBtn.addEventListener('click',signInForm)
+signInBtn.addEventListener('click', signInForm)
 
 signUpBtn.addEventListener('click', signUpForm)
 
 signOutBtn.addEventListener('click', () => {
     fetch("http://localhost:3000/logout", {
-        method: "DELETE",  
-      })
+        method: "DELETE",
+    })
         .then(res => res.json())
         .then((data) => console.log(data))
-        localStorage.clear()
-        location.reload()
+    localStorage.clear()
+    location.reload()
 })
 
-userBtn.addEventListener('click', ()=>{
-    userStuff.style.display='flex'
-    
+userBtn.addEventListener('click', () => {
+    userStuff.style.display = 'flex'
+
 })
-userStuffClose.addEventListener('click',()=>{
-    userStuff.style.display='none'
+userStuffClose.addEventListener('click', () => {
+    userStuff.style.display = 'none'
 })
-myOrder.addEventListener('click',()=>{
+myOrder.addEventListener('click', () => {
 
     window.location.href = "http://localhost:3000/myOrders.html"
 
